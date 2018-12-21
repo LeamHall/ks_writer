634d2160e2da174ee59900d1908741c89420c461
Leam Hall <leamhall@gmail.com>
2018-12-21 09:11:48 -0500

    First draft of kvm_writer.pl

---
+++ input/setup_new_virtual.sh
@@ -0,0 +1,1 @@
+virt-install -n HOSTNAME -r 2048 --vcpus=1 -v --network=bridge:br0   --disk path=/opt/virtuals/guests/HOSTNAME.img,size=10 --location /opt/iso/CentOS-VERSION.iso --nographics --extra-args="ks=http://192.168.1.200/ks/HOSTNAME_ks.cfg ip=IP netmask=255.255.255.0 console=tty0 console=ttyS0,115200n8" 
---
+++ kvm_writer.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/env perl
+#
+# kvm_writer.pl
+#
+# Usage:
+#   ./kvm_writer.pl -c data/ks_data.csv
+#
+# Notes: 
+#   Edit the files in input/ to suit your environment.
+
+use strict;
+use warnings;
+use 5.008_004;
+use Getopt::Long;
+
+my $version = '7';
+my $config;
+my $network;
+my $host_ip;
+my $gateway;
+my $hostname;
+my $ip;
+my $gw;
+my $input_dir = 'input';
+my $output_dir = 'output';
+my $kvm_file;
+
+GetOptions(
+  "config=s"  => \$config,
+  );
+
+open my $CONFIG,  '<', "$config" or die $!;
+while (<$CONFIG>) {
+  next if ( $_ =~ m/\#/);
+  chomp;
+  my ($version, $network, $host_ip, $gateway, $hostname) = split(':'); 
+ 
+  open my $BASE_FILE, '<', "$input_dir/setup_new_virtual.sh" or die $!;
+  my $kvm_string = do { local $/; <$BASE_FILE> };
+  close $BASE_FILE;
+ 
+  $kvm_file = "setup_" . $hostname . "_virtual.sh"; 
+  open my $OUTPUT, '>', "$output_dir/$kvm_file" or die $!;
+  select $OUTPUT;
+
+  $ip = $network . '.' . $host_ip;
+  $kvm_string =~ s/VERSION/$version/;
+  $kvm_string =~ s/IP/$ip/;
+  $kvm_string =~ s/HOSTNAME/$hostname/g;
+  print $kvm_string;
+
+  close $OUTPUT;
+}
+close $CONFIG;
